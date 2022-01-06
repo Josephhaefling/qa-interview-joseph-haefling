17216a7d0a2a5e4d00e73d3295f680fe6a5dd7f2
Joe Haefling <josephhaefling@gmail.com>
2022-01-06 09:00:46 -0700

    Complete steps 1-3

--- features/example.feature
+++ features/example.feature
@@ -9,3 +9,17 @@ Feature: An Example Feature
     Then    Should have the text input field for lastInitial
 
     Then    Should have checkbox input field
+
+    Then    Should have a Suggest Male Rap Name button
+
+    Then    Should have a Suggest Female Rap Name button
+
+    Then    Should have no content in the firstname text input
+
+    Then    Should have no content in the last initial text input
+
+    Then    Should add content to the firstname feild
+
+    Then    Should add content to the last initial feild
+
+    Then    Should request a male rap name
--- features/step_definitions/example_steps.rb
+++ features/step_definitions/example_steps.rb
@@ -14,7 +14,43 @@ Then(/^Should have the text input field for lastInitial$/) do
   # binding.pry
 end
 
-Then(/^Should have checkbox input field$/)do
-
+Then(/^Should have checkbox input field$/) do
+    page.has_field?('shorten', type: 'checkbox')
 end
+
+Then(/^Should have a Suggest Male Rap Name button$/) do
+    page.has_field?('Male', type: 'submit')
+end
+
+Then(/^Should have a Suggest Female Rap Name button$/) do
+    page.has_field?('Female', type: 'submit')
+end
+
+Then(/^Should have no content in the firstname text input/) do
+  find_field('firstname')
+  has_no_text?(:any, 'hi there')
+end
+
+Then(/^Should have no content in the last initial text input/) do
+  find_field('lastinitial')
+  has_no_text?(:any, 'hi there')
+end
+
+Then(/^Should add content to the firstname feild/) do
+  fill_in('firstname', with: 'Jimbo')
+  has_text?(:any, 'Jimbo')
+end
+
+Then(/^Should add content to the last initial feild/) do
+  fill_in('lastinitial', with: 'B')
+  has_text?(:any, 'B')
+end
+
+Then(/^Should request a male rap name/) do
+  click_button('Male')
+  page.all(:xpath, './/html/body/table/tbody/tr/td/table/tbody/tr/td/div/table/tbody/tr')
+  # page.assert_selector('tr', count: 2)
+end
+
+
 
